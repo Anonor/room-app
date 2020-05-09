d490a0bf82b798cc81307b2d900c4db3f1468544
anonor <13258376216@163.com>
2020-05-09 17:41:51 +0800

    商家管理业务逻辑层

--- room-app-service/src/main/java/room/service/MerchantService.java
+++ room-app-service/src/main/java/room/service/MerchantService.java
@@ -1,4 +1,31 @@
 package room.service;
 
+import room.pojo.Merchant;
+
 public interface MerchantService {
+
+    //根据account查询id
+    public Integer queryIdByAccount(Integer account);
+
+    //根据商家id查询商家信息
+    public Merchant queryMerchantById(Integer merchantId);
+
+    //根据account查询商家信息
+    public Merchant queryMerchantByAccount(Integer account);
+
+    //判断account是否存在（true：存在）
+    public boolean isAccountExist(Integer account);
+
+    //增加（注册）商家信息
+    public Merchant createMerchant(Merchant merchant);
+
+    //登录
+    public boolean queryMerchantForLogin(Integer account, String password);
+
+    //根据商家id修改商家信息（选择性修改，需要有商家id）
+    public void updateMerchant(Merchant merchant);
+
+    //根据商家id删除商家信息
+    public void deleteMerchant(Integer merchantId);
+
 }
--- room-app-service/src/main/java/room/service/impl/MerchantServiceImpl.java
+++ room-app-service/src/main/java/room/service/impl/MerchantServiceImpl.java
@@ -2,8 +2,12 @@ package room.service.impl;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 import room.mapper.MerchantMapper;
+import room.pojo.Merchant;
 import room.service.MerchantService;
+import tk.mybatis.mapper.entity.Example;
 
 @Service
 public class MerchantServiceImpl implements MerchantService {
@@ -11,6 +15,73 @@ public class MerchantServiceImpl implements MerchantService {
     @Autowired
     private MerchantMapper merchantMapper;
 
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Integer queryIdByAccount(Integer account) {
+        Example merchantExample = new Example(Merchant.class);
+        Example.Criteria merchantCriteria = merchantExample.createCriteria();
+        merchantCriteria.andEqualTo("account", account);
+        Merchant merchant = merchantMapper.selectOneByExample(merchantExample);
+        return merchant.getMerchantId();
+    }
 
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Merchant queryMerchantById(Integer merchantId) {
+        Merchant merchant = merchantMapper.selectByPrimaryKey(merchantId);
+        merchant.setPassword(null);
+        return merchant;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public Merchant queryMerchantByAccount(Integer account) {
+        Example merchantExample = new Example(Merchant.class);
+        Example.Criteria merchantCriteria = merchantExample.createCriteria();
+        merchantCriteria.andEqualTo("account", account);
+        Merchant merchant = merchantMapper.selectOneByExample(merchantExample);
+        merchant.setPassword(null);
+        return merchant;
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public boolean isAccountExist(Integer account) {
+        Example merchantExample = new Example(Merchant.class);
+        Example.Criteria merchantCriteria = merchantExample.createCriteria();
+        merchantCriteria.andEqualTo("account", account);
+        Merchant merchant = merchantMapper.selectOneByExample(merchantExample);
+        return merchant == null ? false : true;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public Merchant createMerchant(Merchant merchant) {
+        merchantMapper.insert(merchant);
+        return queryMerchantByAccount(merchant.getAccount());
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    @Override
+    public boolean queryMerchantForLogin(Integer account, String password) {
+        Example merchantExample = new Example(Merchant.class);
+        Example.Criteria merchantCriteria = merchantExample.createCriteria();
+        merchantCriteria.andEqualTo("account", account);
+        merchantCriteria.andEqualTo("password", password);
+        Merchant merchant = merchantMapper.selectOneByExample(merchantExample);
+        return merchant == null ? false : true;
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void updateMerchant(Merchant merchant) {
+        merchantMapper.updateByPrimaryKeySelective(merchant);
+    }
+
+    @Transactional(propagation = Propagation.REQUIRED)
+    @Override
+    public void deleteMerchant(Integer merchantId) {
+        merchantMapper.deleteByPrimaryKey(merchantId);
+    }
 
 }
