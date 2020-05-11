6f8d3087effb426c9c3696c5d2df3a8669c11793
anonor <13258376216@163.com>
2020-05-11 13:58:42 +0800

    完成民宿管理业务逻辑--Anonor

--- room-app-service/src/main/java/room/service/PensionService.java
+++ room-app-service/src/main/java/room/service/PensionService.java
@@ -1,4 +1,37 @@
 package room.service;
 
+import room.pojo.Pension;
+
+import java.util.List;
+
 public interface PensionService {
+
+    //该商家该民宿是否存在（true：存在， false：不存在）
+    public boolean isPensionExist(Integer merchantId, String pensionName);
+
+    //创建民宿（默认设置状态为0）
+    public void createPension(Pension pension);
+
+    //获取所有民宿信息
+    public List<Pension> queryAll();
+
+    //根据商家Id获取所有民宿信息
+    public List<Pension> queryByMerchantId(Integer merchantId);
+
+    //根据民宿Id获取民宿信息
+    public Pension queryByPensionId(Integer pensionId);
+
+    //根据商家Id删除所有民宿
+    public void deletePensionByMerchantId(Integer merchantId);
+
+    //根据民宿Id删除民宿
+    public void deletePensionByPensionId(Integer pensionId);
+
+    //修改民宿信息（民宿id必须存在，其它的选择性存在，要改的存在，注意商家id不能修改，不要赋值）
+    public void updatePension(Pension pension);
+
+    //根据民宿Id修改民宿状态（入住->空房     空房->入住）
+    public void updatePensionStatus(Integer pensionId);
+
+
 }
--- room-app-service/src/main/java/room/service/impl/PensionServiceImpl.java
+++ room-app-service/src/main/java/room/service/impl/PensionServiceImpl.java
@@ -3,7 +3,11 @@ package room.service.impl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import room.mapper.PensionMapper;
+import room.pojo.Pension;
 import room.service.PensionService;
+import tk.mybatis.mapper.entity.Example;
+
+import java.util.List;
 
 @Service
 public class PensionServiceImpl implements PensionService {
@@ -11,6 +15,68 @@ public class PensionServiceImpl implements PensionService {
     @Autowired
     private PensionMapper pensionMapper;
 
+    @Override
+    public boolean isPensionExist(Integer merchantId, String pensionName) {
+        Example pansionExample = new Example(Pension.class);
+        Example.Criteria criteria = pansionExample.createCriteria();
+        criteria.andEqualTo("merchantId", merchantId);
+        criteria.andEqualTo("name", pensionName);
+        Pension pension = pensionMapper.selectOneByExample(pansionExample);
+        return pension == null ? false : true;
+    }
 
+    @Override
+    public void createPension(Pension pension) {
+        pension.setStatus(0);
+        pensionMapper.insert(pension);
+    }
 
+    @Override
+    public List<Pension> queryAll() {
+        return pensionMapper.selectAll();
+    }
+
+    @Override
+    public List<Pension> queryByMerchantId(Integer merchantId) {
+        Example pansionExample = new Example(Pension.class);
+        Example.Criteria criteria = pansionExample.createCriteria();
+        criteria.andEqualTo("merchantId", merchantId);
+        return pensionMapper.selectByExample(pansionExample);
+    }
+
+    @Override
+    public Pension queryByPensionId(Integer pensionId) {
+        return pensionMapper.selectByPrimaryKey(pensionId);
+    }
+
+    @Override
+    public void deletePensionByMerchantId(Integer merchantId) {
+        Example pansionExample = new Example(Pension.class);
+        Example.Criteria criteria = pansionExample.createCriteria();
+        criteria.andEqualTo("merchantId", merchantId);
+        pensionMapper.deleteByExample(pansionExample);
+    }
+
+    @Override
+    public void deletePensionByPensionId(Integer pensionId) {
+        pensionMapper.deleteByPrimaryKey(pensionId);
+    }
+
+    @Override
+    public void updatePension(Pension pension) {
+        pensionMapper.updateByPrimaryKeySelective(pension);
+    }
+
+    @Override
+    public void updatePensionStatus(Integer pensionId) {
+        Pension pension = queryByPensionId(pensionId);
+        int status = pension.getStatus();
+        if (status == 0) {
+            pension.setStatus(1);
+            updatePension(pension);
+        } else if (status == 1) {
+            pension.setStatus(0);
+            updatePension(pension);
+        }
+    }
 }
